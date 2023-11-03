236b337ef87baf822f04851ff5adbe72726bae0d
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2023-11-03 08:34:19 -0600

    Updating json input in Application class to match the correct structure of AnimalModel

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,7 +6,7 @@ public class Application {
      */
     public static void main(String[] args) {
         Lab lab = new Lab();
-        String json = "{ \"name\":\"whiskers\", \"name\":\"cat\", \"age\":4 }";
+        String json = "{ \"name\":\"whiskers\", \"species\":\"cat\", \"age\":4 }";
         System.out.println("Attempting deserialization. Here are the deserialized values of the above JSON: ");
         AnimalModel animalModel = lab.deserializeJSON(json);
         System.out.println(animalModel.getName());
